bb132b4009e96ac9a888ab8b3b22e0d061906ac4
priyadarshani <priyarrsdce@gmail.com>
2023-10-24 21:23:34 +0530

    day6

---
+++ index5.js
@@ -0,0 +1,21 @@
+// Memory
+
+// Stack memory (Primitive), Heap (Non-Primitive)
+
+// let myYoutubename = "abc";
+// let anothername = myYoutubename;
+// anothername = "rrsdce";
+
+// console.log(myYoutubename);
+// console.log(anothername);
+
+// let user1 = {
+//     email : "priyarrsdce@gamil.com",
+//     name : "shhee"
+// }
+
+// let user2 = user1;
+// user2.email = "ganjdcnndbhj@gamil.com"
+
+// console.log(user1.email);
+// console.log(user2.email);
---
+++ index6.js
@@ -0,0 +1,38 @@
+ // String :
+
+//  const name ="priya";
+//  const repoCount = 50;
+
+//  console.log(name + repoCount + " Value");
+
+//  console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const gameName = new String("Priya"); // learn string methods
+ 
+//  console.log(gameName[0]);
+//  console.log(gameName.__proto__);
+
+// console.log(gameName.length); // 5
+//  console.log(gameName.toUpperCase()); // PRIYA
+//  console.log(gameName.charAt(2)); // i
+//  console.log(gameName.indexOf('y')); // 3
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const str1 = "       priya    ";
+console.log(str1.trim());
+
+const url = "https://priya.com/priya%20darshani"
+console.log(url.replace('%20', '-'));
+console.log(url.includes("priya"))
+
+// convert string into array based on 
+2 : 29
+
+
+ // How many are there in string
+
